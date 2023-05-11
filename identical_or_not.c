f2204c214c3892cfd64d43eb862d46b689ae54cd
Vidhi Ajmera <113877644+Vidhi-Ajmera16355@users.noreply.github.com>
2023-05-12 00:39:05 +0530

    Create identical_or_not.c

---
+++ identical_or_not.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+int main() {
+    char s1[100];
+    printf("Enter string1:\n");
+    scanf("%[^\n]c",s1);
+    char s2[100];
+    printf("Enter string2:\n");
+    scanf("%s",s2);
+    int n = 0;
+    int flag=1;
+    while (s1[n] !='\0'){
+        if (s1[n] != s2[n]){
+            flag=0;
+            break;
+        }
+        n++;
+    }
+    if (flag==1){
+        printf("%s and %s are identical\n",s1,s2);
+    }
+    else {
+        printf("%s and %s are not identical\n",s1,s2);
+    }
+    return 0;
+}
